102d6441c0b26d0ebf480f16f94949b40efafcfd
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-04-30 21:46:59 +0300

    chang some color in app color

---
+++ lib/core/customformfield.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+import 'package:market/core/app_color.dart';
+
+class CustomTextFormField extends StatelessWidget {
+  const CustomTextFormField({
+    super.key,
+    this.hintText,
+    this.suffixIcon,
+    this.labelText,
+    this.isPassword = false,
+  });
+  final String? hintText;
+  final Widget? suffixIcon;
+  final String? labelText;
+  final bool isPassword;
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      obscuringCharacter: "*",
+      obscureText: isPassword,
+      validator: (value) {
+        if (value == null || value.isEmpty) {
+          return "Please enter your email";
+        }
+        return null;
+      },
+      keyboardType: TextInputType.emailAddress,
+      decoration: InputDecoration(
+        labelText: "$labelText",
+        hintText: "$hintText",
+        suffixIcon: suffixIcon,
+        border: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(10),
+          borderSide: BorderSide(color: AppColor.kBordersideColor, width: 1),
+        ),
+      ),
+    );
+  }
+}
--- lib/views/auth/ui/login_view.dart
+++ lib/views/auth/ui/login_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:market/core/app_color.dart';
+import 'package:market/core/customformfield.dart';
 
 class LoginView extends StatelessWidget {
   const LoginView({Key? key}) : super(key: key);
@@ -8,12 +9,157 @@ class LoginView extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       home: Scaffold(
-      appBar: AppBar(
-        title: const Text('Login'),
+        backgroundColor: AppColor.kScaffoldColor,
+        body: ListView(
+          children: [
+            Container(
+              color: AppColor.kScaffoldColor,
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(height: 75),
+                  Text(
+                    "Welcom To Mo Market",
+                    style: TextStyle(
+                      fontSize: 30,
+                      fontWeight: FontWeight.bold,
+                      color: AppColor.kBlackColor,
+                    ),
+                  ),
+                  Container(
+                    decoration: BoxDecoration(
+                      color: AppColor.kWhiteColor,
+                      borderRadius: BorderRadius.circular(25),
+                    ),
+                    margin: EdgeInsets.all(35),
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      children: [
+                        SizedBox(height: 20),
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          child: CustomTextFormField(
+                            hintText: "Email",
+                            labelText: "Email",
+                          ),
+                        ),
+                        Container(
+                          margin: EdgeInsets.all(20),
+                          child: CustomTextFormField(
+                            
+                            isPassword: true,
+                            hintText: "Password",
+                            labelText: "Password",
+                            suffixIcon: IconButton(
+                              onPressed: () {},
+                              icon: IconButton(
+                                onPressed: () {},
+                                icon: Icon(Icons.visibility_off_outlined),
+                              ),
+                            ),
+                          ),
+                        ),
+                        InkWell(
+                          onTap: () {},
+                          child: Container(
+                            margin: EdgeInsets.only(left: 170),
+                            child: Text(
+                              textAlign: TextAlign.end,
+                              "Forget Password?",
+                              style: TextStyle(
+                                color: AppColor.kPrimaryColor,
+                                fontSize: 13,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                          ),
+                        ),
+                        SizedBox(height: 25),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                          children: [
+                            Text(
+                              "Login",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: AppColor.kBlackColor,
+                              ),
+                            ),
+                            MaterialButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              color: AppColor.kPrimaryColor,
+                              onPressed: () {},
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: AppColor.kWhiteColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+
+                          children: [
+                            Text(
+                              "Login With Google",
+                              style: TextStyle(
+                                fontSize: 20,
+                                fontWeight: FontWeight.bold,
+                                color: AppColor.kBlackColor,
+                              ),
+                            ),
+                            MaterialButton(
+                              shape: RoundedRectangleBorder(
+                                borderRadius: BorderRadius.circular(20),
+                              ),
+                              color: AppColor.kPrimaryColor,
+                              onPressed: () {},
+                              child: Icon(
+                                Icons.arrow_forward,
+                                color: AppColor.kWhiteColor,
+                              ),
+                            ),
+                          ],
+                        ),
+                        SizedBox(height: 20),
+                        Row(
+                          mainAxisAlignment: MainAxisAlignment.center,
+                          children: [
+                            Text(
+                              "Don't have an account?",
+                              style: TextStyle(
+                                fontSize: 18,
+                                color: AppColor.kBlackColor,
+                                fontWeight: FontWeight.bold,
+                              ),
+                            ),
+                            InkWell(
+                              onTap: () {},
+                              child: Text(
+                                "Sign Up",
+                                style: TextStyle(
+                                  color: AppColor.kPrimaryColor,
+                                  fontSize: 15,
+                                  fontWeight: FontWeight.bold,
+                                ),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ],
+        ),
       ),
-      body: Center(
-        child: const Text('Login View'),
-      ),
-    ));
+    );
   }
 }
