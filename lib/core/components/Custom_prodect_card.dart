8f9381b7384ebe01bdc9706908cee15db9811bcb
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-05-07 22:03:32 +0300

    finish home view ui

---
+++ lib/core/components/Custom_prodect_card.dart
@@ -0,0 +1,118 @@
+import 'package:flutter/material.dart';
+import 'package:market/core/app_color.dart';
+
+class CutomProductCard extends StatelessWidget {
+  const CutomProductCard({
+    super.key,
+    required this.imgUrl,
+    required this.productName,
+    required this.productPrice,
+    required this.productDiscountPrice,
+  });
+  final String imgUrl;
+  final String productName;
+  final String productPrice;
+  final String productDiscountPrice;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(16)),
+      child: Column(
+        children: [
+          SizedBox(height: 10),
+          Stack(
+            children: [
+              ClipRRect(
+                borderRadius: BorderRadius.only(
+                  topRight: Radius.circular(16),
+                  bottomLeft: Radius.circular(16),
+                  bottomRight: Radius.circular(16),
+                ),
+                child: Image.network(imgUrl),
+              ),
+              Positioned(
+                child: Container(
+                  width: 65,
+                  height: 40,
+                  alignment: Alignment.center,
+                  decoration: BoxDecoration(
+                    color: AppColor.kPrimaryColor,
+                    borderRadius: BorderRadius.only(
+                      topRight: Radius.circular(16),
+                      bottomRight: Radius.circular(16),
+                    ),
+                  ),
+                  child: Text(
+                    "10% off",
+                    style: TextStyle(color: AppColor.kWhiteColor),
+                  ),
+                ),
+              ),
+            ],
+          ),
+          SizedBox(height: 15),
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text(
+                      " $productName",
+                      style: TextStyle(
+                        fontSize: 18,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite, color: AppColor.kGreyColor),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 5),
+
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Column(
+                      children: [
+                        Text(
+                          "$productPrice",
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+
+                        Text(
+                          "$productDiscountPrice",
+                          style: TextStyle(
+                            decoration: TextDecoration.lineThrough,
+                            color: AppColor.kGreyColor,
+                          ),
+                        ),
+                      ],
+                    ),
+                    MaterialButton(
+                      height: 40,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(16),
+                      ),
+                      onPressed: () {},
+                      child: Text("Buy Now"),
+                      color: AppColor.kPrimaryColor,
+                      textColor: AppColor.kWhiteColor,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/views/home/ui/home_view.dart
+++ lib/views/home/ui/home_view.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:market/core/app_color.dart';
+import 'package:market/core/components/Custom_prodect_card.dart';
 import 'package:market/core/components/custom_search_field.dart';
 import 'package:market/views/home/ui/widget/categories_list.dart';
 
@@ -23,109 +24,27 @@ class HomeView extends StatelessWidget {
           SizedBox(height: 15),
           CategoriesList(),
           SizedBox(height: 15),
-          Card(
-            shape: RoundedRectangleBorder(
-              borderRadius: BorderRadius.circular(16),
+          Container(
+            alignment: Alignment.centerLeft,
+            child: Text(
+              "Recently Added",
+              style: TextStyle(fontSize: 24, fontWeight: FontWeight.bold),
             ),
-            child: Column(
-              children: [
-                Stack(
-                  children: [
-                    ClipRRect(
-                      borderRadius: BorderRadius.only(
-                        topRight: Radius.circular(16),
-                        bottomLeft: Radius.circular(16),
-                        bottomRight: Radius.circular(16),
-                      ),
-                      child: Image.network(
-                        "https://img.freepik.com/premium-psd/scifi-futuristic-room-podium-product-display_401134-692.jpg?ga=GA1.1.1613592830.1746549167&semt=ais_hybrid&w=740",
-                      ),
-                    ),
-                    Positioned(
-                      child: Container(
-                        width: 65,
-                        height: 40,
-                        alignment: Alignment.center,
-                        decoration: BoxDecoration(
-                          color: AppColor.kPrimaryColor,
-                          borderRadius: BorderRadius.only(
-                            topRight: Radius.circular(16),
-                            bottomRight: Radius.circular(16),
-                          ),
-                        ),
-                        child: Text(
-                          "10% off",
-                          style: TextStyle(color: AppColor.kWhiteColor),
-                        ),
-                      ),
-                    ),
-                  ],
-                ),
-                SizedBox(height: 15),
-                Padding(
-                  padding: const EdgeInsets.all(8.0),
-                  child: Column(
-                    children: [
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Text(
-                            "Product Name",
-                            style: TextStyle(
-                              fontSize: 18,
-                              fontWeight: FontWeight.bold,
-                            ),
-                          ),
-                          IconButton(
-                            onPressed: () {},
-                            icon: Icon(
-                              Icons.favorite,
-                              color: AppColor.kGreyColor,
-                            ),
-                          ),
-                        ],
-                      ),
-                      SizedBox(height: 5),
-
-                      Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                        children: [
-                          Column(
-                            children: [
-                              Text(
-                                "100\$",
-                                style: TextStyle(
-                                  fontSize: 18,
-                                  fontWeight: FontWeight.bold,
-                                ),
-                              ),
-
-                              Text(
-                                "125\$",
-                                style: TextStyle(
-                                  decoration: TextDecoration.lineThrough,
-                                  color: AppColor.kGreyColor,
-                                ),
-                              ),
-                            ],
-                          ),
-                          MaterialButton(
-                            height: 40,
-                            shape: RoundedRectangleBorder(
-                              borderRadius: BorderRadius.circular(16),
-                            ),
-                            onPressed: () {},
-                            child: Text("Buy Now"),
-                            color: AppColor.kPrimaryColor,
-                            textColor: AppColor.kWhiteColor,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                ),
-              ],
-            ),
+          ),
+          SizedBox(height: 10),
+          ListView.builder(
+            shrinkWrap: true,
+            physics: NeverScrollableScrollPhysics(),
+            itemCount: 10,
+            itemBuilder: (context, index) {
+              return CutomProductCard(
+                imgUrl:
+                    "https://img.freepik.com/free-photo/modern-comfortable-workplace-home-there-are-computer-laptop-table_613910-13268.jpg?ga=GA1.1.164920025.1746638074&semt=ais_hybrid&w=740",
+                productName: "Product Name",
+                productPrice: "\$100",
+                productDiscountPrice: "\$90",
+              );
+            },
           ),
         ],
       ),
