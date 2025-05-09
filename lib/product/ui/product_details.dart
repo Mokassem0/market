dfca86413368c64bf11f22261eb50dfae9f03fad
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-05-09 21:50:41 +0300

    add reating bar for product details view

--- lib/product/ui/product_details.dart
+++ lib/product/ui/product_details.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:market/core/components/function/AppBar.dart';
 
 class ProductDetails extends StatelessWidget {
@@ -10,9 +11,62 @@ class ProductDetails extends StatelessWidget {
       appBar: buildCustomAppBar(context, "Product Name"),
       body: ListView(
         children: [
-          Image.network("https://img.freepik.com/free-photo/modern-comfortable-workplace-home-there-are-computer-laptop-table_613910-13268.jpg?ga=GA1.1.164920025.1746638074&semt=ais_hybrid&w=740")
+          Image.network(
+            "https://img.freepik.com/free-photo/modern-comfortable-workplace-home-there-are-computer-laptop-table_613910-13268.jpg?ga=GA1.1.164920025.1746638074&semt=ais_hybrid&w=740",
+          ),
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 30),
+            child: Column(
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Text("Product Name", style: TextStyle(fontSize: 18)),
+                    Text("\$100", style: TextStyle(fontSize: 18)),
+                  ],
+                ),
+                SizedBox(height: 20),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    Row(
+                      children: [
+                        Text("3 "),
+                        Icon(Icons.star, color: Colors.amber),
+                      ],
+                    ),
+                    IconButton(
+                      onPressed: () {},
+                      icon: Icon(Icons.favorite, color: Colors.grey),
+                    ),
+                  ],
+                ),
+                SizedBox(height: 25),
+                Center(
+                  child: Text(
+                    "Product Description",
+                    style: TextStyle(fontSize: 16),
+                  ),
+                ),
+                SizedBox(height: 20),
+                RatingBar.builder(
+                  initialRating: 3,
+                  minRating: 1,
+                  direction: Axis.horizontal,
+                  allowHalfRating: true,
+                  itemCount: 5,
+                  itemPadding: EdgeInsets.symmetric(horizontal: 4.0),
+                  itemBuilder:
+                      (context, _) => Icon(Icons.star, color: Colors.amber),
+                  onRatingUpdate: (rating) {
+                    print(rating);
+                  },
+                ),
+              ],
+            ),
+          ),
         ],
-      )
+      ),
     );
   }
 }
