bf558dc2971ea8c5fba2fe5de00832bc16ccb9a9
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-05-10 20:41:15 +0300

    intialize supabase in our project

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:market/views/auth/ui/login_view.dart';
 import 'package:market/views/nav_bar/ui/main_home_view.dart';
+import 'package:supabase_flutter/supabase_flutter.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Supabase.initialize(
+    url: 'https://vloghozftpjfwiypbtup.supabase.co',
+    anonKey:
+        'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InZsb2dob3pmdHBqZndpeXBidHVwIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDY4OTc5MDksImV4cCI6MjA2MjQ3MzkwOX0.HGBhmzokzJN7ZLQbgtRUmRDAT6k6DuyxVSrGpCZ7iAE',
+  );
   runApp(Market());
 }
 
