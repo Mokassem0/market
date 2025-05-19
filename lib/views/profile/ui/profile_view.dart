cc6de770147721994127e66a5f6dc3b5ea5bb7e1
Mokassem0 <107583487+Mokassem0@users.noreply.github.com>
2025-05-19 21:30:59 +0300

    fix get user data

--- lib/views/profile/ui/profile_view.dart
+++ lib/views/profile/ui/profile_view.dart
@@ -13,95 +13,101 @@ class ProfileView extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    UserDataModel user = context.read<AuthenticationCubit>().userDataModel!;
-    return BlocConsumer<AuthenticationCubit, AuthenticationState>(
-      listener: (context, state) {
-        if (state is LogoutSuccess) {
-          Navigator.of(context).pushReplacement(
-            MaterialPageRoute(builder: (context) => LoginView()),
-          );
-        }
-        if (state is LogoutError) {
-          ScaffoldMessenger.of(
-            context,
-          ).showSnackBar(SnackBar(content: Text(state.message)));
-        }
-      },
-      builder: (context, state) {
-        return state is LogoutLoading
-            ? Center(child: CircularProgressIndicator())
-            : Scaffold(
-              backgroundColor: AppColor.kScaffoldColor,
-              body: ListView(
-                children: [
-                  SizedBox(height: 75),
-                  Container(
-                    padding: const EdgeInsets.all(15),
-                    color: AppColor.kWhiteColor,
-                    child: Column(
-                      children: [
-                        CircleAvatar(
-                          radius: 50,
-                          backgroundColor: AppColor.kPrimaryColor,
-                          foregroundColor: AppColor.kWhiteColor,
-                          child: Icon(
-                            Icons.person,
-                            size: 50,
-                            color: AppColor.kWhiteColor,
-                          ),
-                        ),
-                        SizedBox(height: 20),
-                        Text(
-                          user.name ?? "user.name",
-                          style: TextStyle(
-                            fontSize: 24,
-                            fontWeight: FontWeight.bold,
-                          ),
-                        ),
-                        SizedBox(height: 10),
-                        Text(
-                          user.email ?? "User Eamil",
-                          style: TextStyle(fontSize: 24),
-                        ),
-                        SizedBox(height: 10),
-                        CustomRowCardBtn(
-                          title: "Edit Name",
-                          icon: Icons.person,
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => EditName(),
-                              ),
-                            );
-                          },
-                        ),
-                        SizedBox(height: 10),
-                        CustomRowCardBtn(
-                          title: "My Orders",
-                          icon: Icons.shopping_cart,
-                          onTap: () {
-                            Navigator.of(context).push(
-                              MaterialPageRoute(
-                                builder: (context) => MyOrder(),
-                              ),
-                            );
-                          },
-                        ),
-                        SizedBox(height: 10),
-                        CustomRowCardBtn(
-                          title: "Logout",
-                          icon: Icons.logout,
-                          onTap: () async {
-                            await context.read<AuthenticationCubit>().signOut();
-                          },
-                        ),
-                      ],
-                    ),
-                  ),
-                ],
-              ),
+    return BlocProvider(
+      create: (context) => AuthenticationCubit()..getUserData(),
+      child: BlocConsumer<AuthenticationCubit, AuthenticationState>(
+        listener: (context, state) {
+          if (state is LogoutSuccess) {
+            Navigator.of(context).pushReplacement(
+              MaterialPageRoute(builder: (context) => LoginView()),
             );
-      },
+          }
+          if (state is LogoutError) {
+            ScaffoldMessenger.of(
+              context,
+            ).showSnackBar(SnackBar(content: Text(state.message)));
+          }
+        },
+        builder: (context, state) {
+          UserDataModel? user =
+              context.read<AuthenticationCubit>().userDataModel;
+          return state is LogoutLoading || state is GetUserDataLoading
+              ? Center(child: CircularProgressIndicator())
+              : Scaffold(
+                backgroundColor: AppColor.kScaffoldColor,
+                body: ListView(
+                  children: [
+                    SizedBox(height: 75),
+                    Container(
+                      padding: const EdgeInsets.all(15),
+                      color: AppColor.kWhiteColor,
+                      child: Column(
+                        children: [
+                          CircleAvatar(
+                            radius: 50,
+                            backgroundColor: AppColor.kPrimaryColor,
+                            foregroundColor: AppColor.kWhiteColor,
+                            child: Icon(
+                              Icons.person,
+                              size: 50,
+                              color: AppColor.kWhiteColor,
+                            ),
+                          ),
+                          SizedBox(height: 20),
+                          Text(
+                            user?.name ?? "user.name",
+                            style: TextStyle(
+                              fontSize: 24,
+                              fontWeight: FontWeight.bold,
+                            ),
+                          ),
+                          SizedBox(height: 10),
+                          Text(
+                            user?.email ?? "User Eamil",
+                            style: TextStyle(fontSize: 24),
+                          ),
+                          SizedBox(height: 10),
+                          CustomRowCardBtn(
+                            title: "Edit Name",
+                            icon: Icons.person,
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => EditName(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(height: 10),
+                          CustomRowCardBtn(
+                            title: "My Orders",
+                            icon: Icons.shopping_cart,
+                            onTap: () {
+                              Navigator.of(context).push(
+                                MaterialPageRoute(
+                                  builder: (context) => MyOrder(),
+                                ),
+                              );
+                            },
+                          ),
+                          SizedBox(height: 10),
+                          CustomRowCardBtn(
+                            title: "Logout",
+                            icon: Icons.logout,
+                            onTap: () async {
+                              await context
+                                  .read<AuthenticationCubit>()
+                                  .signOut();
+                            },
+                          ),
+                        ],
+                      ),
+                    ),
+                  ],
+                ),
+              );
+        },
+      ),
     );
   }
 }
